898ce2e1e46df478d25a459a7bc600d4fb93dc67
oflorez1381 <oscarflorez1381@gmail.com>
2019-07-30 21:56:48 -0400

    06 Manejo de errores en el metodo show

--- clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/controller/ClienteRestController.java
+++ clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/controller/ClienteRestController.java
@@ -2,11 +2,14 @@ package com.odfd.spring.boot.backend.apirest.controller;
 
 import com.odfd.spring.boot.backend.apirest.models.entity.Cliente;
 import com.odfd.spring.boot.backend.apirest.models.service.ClienteService;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -20,24 +23,40 @@ public class ClienteRestController {
     }
 
     @GetMapping
-    public List<Cliente> index(){
+    public List<Cliente> index() {
         return clienteService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Cliente show(@PathVariable Long id){
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException exception) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", exception.getMessage().concat(" : ").concat(exception.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID: ".concat(id.toString()).concat(" no existe en la base de datos"));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
+    public Cliente create(@RequestBody Cliente cliente) {
         return clienteService.save(cliente);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente, @PathVariable  Long id){
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
 
         Cliente clienteActual = clienteService.findById(id);
 
@@ -50,7 +69,7 @@ public class ClienteRestController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         clienteService.delete(id);
     }
 
--- clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/models/repository/ClienteRepository.java
+++ clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/models/repository/ClienteRepository.java
@@ -1,8 +1,7 @@
 package com.odfd.spring.boot.backend.apirest.models.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.odfd.spring.boot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.repository.CrudRepository;
 
 public interface ClienteRepository extends CrudRepository<Cliente,Long> {
 }
