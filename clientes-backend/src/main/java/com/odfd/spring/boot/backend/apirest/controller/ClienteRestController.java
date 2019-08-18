14ef2395040ef6eee5aa983ce0cd190d138b2261
oflorez1381 <oscarflorez1381@gmail.com>
2019-08-17 20:06:57 -0400

    13.15 Anadiendo la foto al listado

--- clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/controller/ClienteRestController.java
+++ clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/controller/ClienteRestController.java
@@ -218,7 +218,15 @@ public class ClienteRestController {
         }
 
         if (!recurso.exists() && !recurso.isReadable()){
-            throw new RuntimeException("Error no se pudo cargar la imagen : " + nombreFoto);
+            rutaArchivo = Paths.get("src/main/resources/static/images").resolve("no-usuario.png").toAbsolutePath();
+
+            try {
+                recurso = new UrlResource(rutaArchivo.toUri());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            log.error("Error no se pudo cargar la imagen : " + nombreFoto);
         }
 
         HttpHeaders cabecera = new HttpHeaders();
