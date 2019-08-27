f2066a8041ac12940e06bf82f3ada15871714c59
oflorez1381 <oscarflorez1381@gmail.com>
2019-08-26 20:07:01 -0400

    15.7 Creando el servicio UsuarioService

--- clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/models/entity/security/Rol.java
+++ clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/models/entity/security/Rol.java
@@ -13,4 +13,20 @@ public class Rol implements Serializable {
 
     @Column(unique = true, length = 20)
     private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
