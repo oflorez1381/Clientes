51c7d23ded1ea9abc8670f6ec6e410bebd9a8336
oflorez1381 <oscarflorez1381@gmail.com>
2019-08-27 20:04:00 -0400

    15.9 Anadiendo la configuracion para el servidor de autorizacion

--- clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/auth/SpringSecurityConfig.java
+++ clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/auth/SpringSecurityConfig.java
@@ -3,11 +3,11 @@ package com.odfd.spring.boot.backend.apirest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,15 +18,21 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         this.usuarioService = usuarioService;
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     @Override
     @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(usuarioService)
                 .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return  new BCryptPasswordEncoder();
     }
 }
