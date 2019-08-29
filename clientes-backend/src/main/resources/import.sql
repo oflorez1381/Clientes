daea8385cbbf7ed90951d247f232840127a75182
oflorez1381 <oscarflorez1381@gmail.com>
2019-08-28 21:59:39 -0400

    15.12 Generando JWT Token

--- clientes-backend/src/main/resources/import.sql
+++ clientes-backend/src/main/resources/import.sql
@@ -3,3 +3,22 @@ INSERT INTO clientes (nombre, apellido, email, created_at, foto) VALUES ('Pedro'
 INSERT INTO clientes (nombre, apellido, email, created_at, foto) VALUES ('Fernando', 'Alonso', 'fernando.alonso@gmail.com', '2007-12-11', '');
 INSERT INTO clientes (nombre, apellido, email, created_at, foto) VALUES ('Vanessa', 'Porras', 'vane.porra@gmail.com', '2018-04-20', '');
 INSERT INTO clientes (nombre, apellido, email, created_at, foto) VALUES ('Angie', 'Vanegas', 'agieva@gmail.com', '2007-12-09', '');
+
+-- INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamérica');
+-- INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamérica');
+-- INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamérica');
+-- INSERT INTO regiones (id, nombre) VALUES (4, 'Europa');
+-- INSERT INTO regiones (id, nombre) VALUES (5, 'Asia');
+-- INSERT INTO regiones (id, nombre) VALUES (6, 'África');
+-- INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía');
+-- INSERT INTO regiones (id, nombre) VALUES (8, 'Antártica');
+
+INSERT INTO usuarios (username,password,enabled) VALUES ('oscar','$2a$10$E9o7Kj9iVxekVg9VjNgfteiVwaA6zL.S1mQWA8d6ZLl2n51jmkxG.',1);
+INSERT INTO usuarios (username,password,enabled) VALUES ('admin','$2a$10$S4pspiz1.ICXFOfCsqdj8uJ4rVA3XnLT4tuzCJ26HBXtvYMONU/pm',1);
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1,1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,1);
+INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,2);
