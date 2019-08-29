5842e6cb815a8ee83088d89b83487f5bac92bb8b
oflorez1381 <oscarflorez1381@gmail.com>
2019-08-28 22:14:09 -0400

    15.13 Asignando la llave secreta MAC para firmar el token JWT

---
+++ clientes-backend/src/main/java/com/odfd/spring/boot/backend/apirest/auth/JwtConfig.java
@@ -0,0 +1,5 @@
+package com.odfd.spring.boot.backend.apirest.auth;
+
+public class JwtConfig {
+    public static final String LLAVE_SECRETA = "alguna.llave.secreta";
+}
--- clientes-backend/src/main/resources/import.sql
+++ clientes-backend/src/main/resources/import.sql
@@ -19,6 +19,6 @@ INSERT INTO usuarios (username,password,enabled) VALUES ('admin','$2a$10$S4pspiz
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
 
-INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1,1);
-INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,1);
-INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,2);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (1,1);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2,1);
+INSERT INTO usuarios_roles (usuario_id, rol_id) VALUES (2,2);
